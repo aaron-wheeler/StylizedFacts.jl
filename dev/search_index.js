6a6a40fb450289d2c47be88744af3ec84bd8b560
Documenter.jl <documenter@juliadocs.github.io>
2023-03-08 17:03:35 +0000

    build based on 7e3cc9a

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = StylizedFacts","category":"page"},{"location":"#StylizedFacts","page":"Home","title":"StylizedFacts","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for StylizedFacts.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [StylizedFacts]","category":"page"}]
+}
